ca94b155634d85b6ccbd0d7b39242a957c1cbebe
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-13 23:31:22 +0530

    Jul 13  23:31:21

---
+++ 10-handtodice-Python/handtodice.py
@@ -0,0 +1,18 @@
+# Write the (very short) function handtodice(hand) that takes a hand, which is a 3-digit
+# integer, and returns 3 values, each of the 3 dice in the hand. For example:
+# assert(handToDice(123) == (1,2,3))
+# assert(handToDice(214) == (2,1,4))
+# assert(handToDice(422) == (4,2,2))
+# Hint: You might find // and % useful here, and also getKthDigit().
+# def getKthDigit(i):
+
+def handtodice(hand):
+	a = set()
+	b = ""+hand
+	b = b.split()
+	while(i<3):
+		a.add()
+		hand = hand//10
+		i -=1
+	# print(a)
+	return a
