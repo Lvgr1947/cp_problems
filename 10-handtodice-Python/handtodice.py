7d5d7d658e09a8b95f69f24cfa053c5c0a123822
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 00:05:44 +0530

    Jul 14  00:05:43

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -6,28 +6,12 @@
 # Hint: You might find // and % useful here, and also getKthDigit().
 # def getKthDigit(i):
 
-# def handtodice(hand):
-# 	a = set()
-# 	b = list(str(hand))
-# 	i = 0
-# 	while(i<3):
-# 		a.add(int(b[i]))
-# 		i += 1
-# 	print(a)
-# 	return a
 def handtodice(hand):
-	# your code goes here
-	s=""
-	num = [int(d) for d in str(hand)]
-	s+="("
-	for x in range(len(num)):
-		s+=(str(num[x]))
-		if(x != len(num)-1):
-			s+=(",")
-	s+=(")")
-	print(s)
-handtodice(123)
-handtodice(214)
-handtodice(422)
-handtodice(400)
-handtodice(101)
+	a = set()
+	b = list(str(hand))
+	i = 0
+	while(i<3):
+		a.add(int(b[i]))
+		i += 1
+	print(a)
+	return a
