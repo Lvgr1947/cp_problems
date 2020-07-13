a3645c9cd76378ca1ca163fead7122066d4cc3f4
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 01:45:29 +0530

    Jul 14  01:45:27

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -4,18 +4,15 @@
 #     y = m2*x + b2
 # This function returns the x value of the point of intersection of the two lines. If the lines are parallel, or identical, the function should return None.
 def fun_ismultiple(m, n):
-	if(n<m):
-		(m,n) = (n,m)
-	if n!=0:
-		if m%n==0:
-			return True
-		else:
-			return False
+	if m%n == 0 or n%m == 0:
+		return False
+	else:
+		return True
 def lineintersection(m1, b1, m2, b2):
 	# your code goes here
 
 	if (m1 != m2 and b1 != b2):
-		if not fun_ismultiple(m1,m2): 
+		if fun_ismultiple(m1,m2): 
 			return ((b1-b2)/(m2-m1))
 		else:
 			return None
