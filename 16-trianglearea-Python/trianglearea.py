64e3c35887f5dcae971c5990c7a05f0c94944bc9
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 01:22:02 +0530

    Jul 14  01:22:01

--- 16-trianglearea-Python/trianglearea.py
+++ 16-trianglearea-Python/trianglearea.py
@@ -13,7 +13,7 @@ def islegaltriangle(s1, s2, s3):
 
 def trianglearea(s1, s2, s3):
 	if islegaltriangle(s1,s2,s3):
-		s = s1+s2+s3
+		s = (s1+s2+s3)/2
 		area = math.sqrt(s*(s-s1)*(s-s2)*(s-s3))
 		return area
 	else:
