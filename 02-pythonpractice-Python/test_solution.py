afb604d534e7350fb6bfc9ce432c231bfb71f852
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-13 21:34:10 +0530

    Jul 13  21:34:10

--- 02-pythonpractice-Python/test_solution.py
+++ 02-pythonpractice-Python/test_solution.py
@@ -34,7 +34,7 @@ me = Classy()
 me.addItem("tophat")
 result = me.classiness()
 
-@pytest.mark.parametrize('result,check',[
+@pytest.mark.parametrize('result','check',[
     (result,2)
 ])
 def test_classiness_3(result, check):
@@ -47,7 +47,7 @@ me.addItem("bowtie")
 me.addItem("jacket")
 me.addItem("monocle")
 result = me.classiness()
-@pytest.mark.parametrize('result,check',[
+@pytest.mark.parametrize('result','check',[
     (result,11)
 ])
 def test_classiness_4(result, check):
