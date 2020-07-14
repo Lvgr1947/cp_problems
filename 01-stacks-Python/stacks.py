d2f66672a3b528e10264cdf7350a8a925bccd899
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 12:44:56 +0530

    Jul 14  12:44:55

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -35,7 +35,7 @@ class LinkedList(object):
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
-        pass
+        
 
 class stack(object):
     def __init__(self,top=None):
