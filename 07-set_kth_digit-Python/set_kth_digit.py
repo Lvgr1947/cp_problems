9f1c5d684c0698388750a2a86a48ff70f02122ba
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 19:42:09 +0530

    Jul 14  19:42:07

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -10,9 +10,10 @@ def fun_set_kth_digit(n, k, d):
 	n = str(n)
 	n1 = list(n)
 	if(len(n1)>k):
-		n1[len-k] = str(d)
+		n1[len(n1)-k] = str(d)
 		n1 = str(n1)
 		return n1
 	else:
 		n = str(d)+n
 		return int(n)
+fun_set_kth_digit(123,2,5)
