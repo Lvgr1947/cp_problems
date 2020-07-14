5e9b116d235a41bab7d3b12d7059cbc05e547e61
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 13:44:20 +0530

    Jul 14  13:44:19

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -16,5 +16,5 @@ class Queue:
 
     def dequeue(self):
         a = self.storage[0]
-        for i in range(len(self.storage)-1):
+        for i in range(len(self.storage)-2):
             self.storage[i] = self.storage[i+1]
