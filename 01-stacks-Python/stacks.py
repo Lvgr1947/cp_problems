3ad5f02d12ac75bb03b1b2f3c08a5b392c5e4f42
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 12:46:25 +0530

    Jul 14  12:46:24

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -44,7 +44,7 @@ class stack(object):
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        pass
+        
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
