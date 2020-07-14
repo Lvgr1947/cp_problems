f7f9ae772e14576e7f9384bf64f77c3010de5b78
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 15:40:37 +0530

    Jul 14  15:40:36

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -23,9 +23,9 @@ def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# 	return True
 	# else:
 	# 	return False
-	m1 = int((y2-y1)/(x2-x1))
-	m2 = int((y3-y1)/(x3-x1))
-	m3 = int((y1-y2)/(x1-x2))
+	m1 = (y2-y1)/(x2-x1)
+	m2 = (y3-y1)/(x3-x1)
+	m3 = (y1-y2)/(x1-x2)
 	if m1*m2 == 1 or m2*m3 == 1 or m3*m1 == 1:
 		return True
 	else:
