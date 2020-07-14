32acf235e8e78bb4d19f15e70041a4b7ef83c65e
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 18:36:50 +0530

    Jul 14  18:36:50

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -9,4 +9,11 @@
 
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	a=[]
+	for i in range(n):
+		a.append([])
+		a[i].append(1)
+		for j in range(1,i):
+			a[i].append(a[i-1][j-1]+a[i-1][j])
+		if(n!=0):
+			a[i].append(1)
