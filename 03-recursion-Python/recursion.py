119b48fbf88d73c3d80da3fbe40afffa98ed2626
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 14:16:07 +0530

    Jul 14  14:16:06

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -4,4 +4,4 @@ Your code should have the same input/output as the
 iterative code in the instructions."""
 
 def get_fib(x):
-    return lambda x: x if x<=1 else fib(x-1)
+    return lambda x: x if x<=1 else fib(x-1) + fib(x-2)
