7f45459c25467841738f7ae5a3592cc1feaa1bcb
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 15:36:29 +0530

    Jul 14  15:36:27

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -19,7 +19,7 @@ def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	C = distance(x2,y2,x3,y3)
 	num = [A,B,C]
 	x,y,z = sorted(num)
-	if (x**2 + y**2 == z**2) or (y**2 + z**2 == x**2) or (z**2 + x):
+	if (x**2 + y**2 == z**2) or (y**2 + z**2 == x**2) or (z**2 + x**2 == y**2):
 		return True
 	else:
 		return False
