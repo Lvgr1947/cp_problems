8bcb873f97a4767e0a34a7249cf399394de4b86c
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-15 00:52:55 +0530

    Jul 15  00:52:54

--- 11-bonusplaythreediceyahtzee-Python/playstep2.py
+++ 11-bonusplaythreediceyahtzee-Python/playstep2.py
@@ -49,7 +49,7 @@ def playstep2(hand, dice):
 		dice = dice//10
 		hand = (sorted(hand,reverse=True))
 		hand = handtodice1(hand)
-		a = [hand,dice]
+		a = [hand,1]
 		a = tuple(a)
 		print(a)
 		return a
