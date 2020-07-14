f8ae8bfe592865bb7a769b441cc6a8c91394809f
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 23:38:05 +0530

    Jul 14  23:38:03

--- 12-playStep2-Python/playstep2.py
+++ 12-playStep2-Python/playstep2.py
@@ -60,7 +60,8 @@ def notMatched(hand):
 def removeHand(hand,a):
 	for i in range(a,len(hand)-1):
 		hand[i] = hand[i+1]
-		return hand
+	hand[len(hand)-1]= None
+	return hand
 
 def playstep2(hand, dice):
 	hand = handtodice(hand)
