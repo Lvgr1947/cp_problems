b1b9379f7d671084533d1e5efa17658ab3a78e3a
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 19:19:11 +0530

    Jul 14  19:19:10

--- 06-get_kth_digit-Python/get_kth_digit.py
+++ 06-get_kth_digit-Python/get_kth_digit.py
@@ -9,7 +9,7 @@ def fun_get_kth_digit(digit, k):
 	digit = str(digit)
 	digit = digit[::-1]
 	digit = list(digit)
-	print(len(digit))
+	# print(len(digit))
 	if(len(digit)>=k):
 		return digit[k]
 	else:
