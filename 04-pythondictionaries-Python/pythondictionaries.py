52460994c471f828953989aa06e94a38c8085224
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 14:36:37 +0530

    Jul 14  14:36:36

--- 04-pythondictionaries-Python/pythondictionaries.py
+++ 04-pythondictionaries-Python/pythondictionaries.py
@@ -30,5 +30,5 @@ American City
 Asian City - Country
 Asian City - Country"""
 
-locations = {'North America': {'USA': ['Mountain View','Atlanta']},'Asia':{'India':['Bangalore'],'China':[Shna]},
+locations = {'North America': {'USA': ['Mountain View','Atlanta']},'Asia':{'India':['Bangalore'],'China':['Shangai']},
             'Africa':{'Egypt':['Cairo']}}
