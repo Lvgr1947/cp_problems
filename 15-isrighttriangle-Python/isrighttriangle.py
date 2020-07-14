182e24332b16d5757a0614d8f3de9b8c1f9e8773
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-14 15:24:45 +0530

    Jul 14  15:24:43

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -10,7 +10,7 @@ import math
 def distance(x, y, a, b):
 	dist = math.sqrt(((a-x)**2) + ((b-y**2)))
 	return dist
-	
+		
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	A = distance(x1,y1,x2,y2)
 	B = distance(x1,y1,x3,y3)
