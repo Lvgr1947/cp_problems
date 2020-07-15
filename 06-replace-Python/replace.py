a54958a190deaa042a258a708a8eff275686d113
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-15 20:27:13 +0530

    Jul 15  20:27:10

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -6,7 +6,7 @@
 
 def fun_replace(s1, s2, s3):
 	s4 = s1.split(s2)
-	if(len(s4[0])+ len(s4[1])== (len(s1))):
+	if(len(s4)==1):
 		return s1
 	else:
 		s = ""
@@ -17,5 +17,5 @@ def fun_replace(s1, s2, s3):
 		else:
 			s = s4[0]+s3+s4[1]
 		return s
-print(fun_replace("helloworld123", "hello", "345"))
+print(fun_replace("helloworld123", "34567", "345"))
 
