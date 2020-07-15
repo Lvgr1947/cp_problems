b5ffd0260af4985234f642af317026248cddd29e
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-15 20:22:53 +0530

    Jul 15  20:22:50

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -6,14 +6,15 @@
 
 def fun_replace(s1, s2, s3):
 	s4 = s1.split(s2)
-	if(len(s4))
-	s = ""
-	if len(s4[0])==0:
-		s = s3+s4[1]
-	elif len(s4[1])==0:
-		s = s4[0]+s3
-	else:
-		s = s4[0]+s3+s4[1]
-	return s
+	if(len(s4[0])+ len(s4[1])== (len(s1))):
+		return s1
+		s = ""
+		if len(s4[0])==0:
+			s = s3+s4[1]
+		elif len(s4[1])==0:
+			s = s4[0]+s3
+		else:
+			s = s4[0]+s3+s4[1]
+		return s
 # print(fun_replace("helloworld123", "hello", "345"))
 
