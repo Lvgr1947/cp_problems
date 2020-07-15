ec9cf24522ed437ef542125d2cb3317a84f618f4
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-15 19:46:07 +0530

    Jul 15  19:46:03

--- 05-interleave-Python/interleave.py
+++ 05-interleave-Python/interleave.py
@@ -21,9 +21,7 @@ def fun_interleave(s1,s2):
 	elif(len(s1)<len(s2)):
 		for i in range(len(s1)):
 			s += s1[i]+s2[i]
-		print(s2[len(s1)*2::1])
-		s += s2[len(s1)*2:len(s2):1]
-		print(s)
+		s += s2[len(s1):len(s2):1]
 		return s
 print(fun_interleave("a#", "cD!f2"))
 # , "ac#D!f2"
