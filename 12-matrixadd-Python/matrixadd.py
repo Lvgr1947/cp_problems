0f04f361ac1664ce10d97c3e8cda7d6bff595b76
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-16 15:08:14 +0530

    Jul 16  15:08:09

--- 12-matrixadd-Python/matrixadd.py
+++ 12-matrixadd-Python/matrixadd.py
@@ -40,4 +40,6 @@ def matrixadd(X, Y):
 		return result
 	else:	
 		return None	
-# print(matrixadd([[1,  2,  3],[4,  5,  6]], [[21, 22, 23], [24, 25, 26]]))
+print(matrixadd([[1,  2,  3],[4,  5,  6]], [[21, 22, 23], [24, 25, 26]]))
+print(matrixadd([[1,  2,  3],[4,  5,  6], [7, 8, 9]], [[1,  2,  3],[4,  5,  6], [7, 8, 9]]))
+print(matrixadd)
