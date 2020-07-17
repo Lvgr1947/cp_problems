da86150a32a80f3ec49bd3482f85f34fc8b8996c
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-17 17:52:57 +0530

    Jul 17  17:52:51

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -45,3 +45,7 @@ class BinaryTree(object):
         # Your code goes here
         pass
 tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
