145d0284ad3f2ef93df9c40383d75cc7b919d9cd
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-17 22:23:55 +0530

    Jul 17  22:23:52

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -5,18 +5,58 @@ class Node(object):
         self.right = None
 
 class BST(object):
+
     def __init__(self, root):
         self.root = Node(root)
 
-    def insert(self, new_val):
-        # Your code goes here
-        pass
+
+    # def insert_bst(self, root, val):
+    #     if root is None: 
+    #             root.value = val 
+    #     else: 
+    #         if self.root.value < val: 
+    #             if self.root.right is None: 
+    #                 self.root.right = val 
+    #             else: 
+    #                 self.insert_bst(self.root.right, val) 
+    #         else: 
+    #             if self.root.left is None: 
+    #                 self.root.left = val 
+    #             else: 
+    #                 self.insert_bst(self.root.left, val)
+
+    def insert(self, d):
+        # self.insert_bst(self.root,new_val)
+        if self.root.value == d:
+            return False
+        elif d < self.root.value:
+            if self.root.left:
+                self.root = self.root.left
+                return self.insert(d)
+            else:
+                self.root.left = Node(d)
+                return True
+        else:
+            if self.root.right:
+                self.root = self.root.right
+                return self.insert(d)
+            else:
+                self.root.right = Node(d)
+                return True
 
     def printSelf(self):
-        # Your code goes here
         pass
+        # if self.root is None:
+        #     print("None")
+        # else:
+        #     print(self.root.left)
+        #     print(self.root.right)
         
-    def search(self, find_val):
-        # Your code goes here
-        pass
-
+    # def search(self, d):
+            
+       
+tree = BST(4)
+(tree.insert(2))
+print(tree.insert(1))
+(tree.insert(3))
+tree.insert(5)
