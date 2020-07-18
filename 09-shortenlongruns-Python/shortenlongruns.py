397907717c9ee63688ce7ca4c829f77409036f1e
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-18 15:54:39 +0530

    check

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -5,10 +5,24 @@
 # For example: 
 # assert(shortenLongRuns([2, 3, 5, 5, 5, 3], 2) == [2, 3, 5, 3]) 
 # assert(shortenLongRuns([2, 3, 5, 5, 5, 3], 3) == [2, 3, 5, 5, 3]) 
-# Note: your function may not just create a copy of L and call the destructive version of this function (below) on 
+# Note: your functio may not just create a copy of L and call the destructive version of this function (below) on 
 # that copy and return it. Instead, you must directly construct the result here.
 
 
 def shortenlongruns(L, k):
 	# Your code goes here
-	pass
+	c = 1
+	a=[]
+	for i in range(0,len(L)):
+		if c<k:
+			a.append(L[i])
+			# print(L[i])
+			print(L[i],i)
+		# print(len(L)-2)
+		if i<len(L)-1 and L[i] == L[i+1]:
+			c += 1
+		else:
+			c = 1
+	print(a)
+	return a
+shortenlongruns([2, 3, 5, 5, 5, 3], 2)
