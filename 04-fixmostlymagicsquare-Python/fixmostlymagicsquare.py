fd8a2cdb94ca7e0798aa901047b37e9350881487
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-20 02:00:53 +0530

    Jul 20  02:00:53

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -51,5 +51,5 @@ def fixmostlymagicsquare(L):
 			if g:
 				return L
 			k += 1
-print(fixmostlymagicsquare([[2, 7, 9], [9, 5, 1], [4, 3, 8]]))
+print(ismostlymagicsquare([[16, 3, 2, 13], [5, 10, 11, 18], [9, 6, 7, 12],[4, 15, 14, 1]]))
 	
