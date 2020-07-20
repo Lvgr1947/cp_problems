e5919c43aa3a97c4f4c86818a432f7cca0e86ac2
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-20 15:11:59 +0530

    Jul 20  15:11:57

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -14,8 +14,10 @@ def onlyEvenDigits(L):
 		r = L % 10
 		if r % 2 == 0:
 			s = s*10 + r
+		if L == 0:
+			return s
 		onlyEvenDigits(L // 10)
-		
+
 	else:
 		return 0
 def fun_recursion_onlyevendigits(l): 
