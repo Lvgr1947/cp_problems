0cae8930b34b77c68efbb4cbab94a7f3e4ae236f
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-21 04:11:51 +0530

    Jul 21  04:11:49

---
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -0,0 +1,45 @@
+# Write the function nthSmithNumber that takes a non-negative int n 
+# and returns the nth Smith number, where a Smith number is a composite (non-prime) 
+# the sum of whose digits are the sum of the digits of its prime factors (excluding 1). 
+# Note that if a prime number divides the Smith number multiple times, its digit sum 
+# is counted that many times. For example, 4 equals 2**2, so the prime factor 2 is 
+# counted twice, thus making 4 a Smith Number.
+# so fun_nthsmithnumber(0) should return 4
+# so fun_nthsmithnumber(1) should return 22
+def sum1(l):
+    for i in l:
+        if i>10:
+            j = list(str(i))
+            
+def isprime(n):
+	if n > 1:
+		for i in range(2,n):
+			if n % i ==0:
+				return False
+		return True
+
+def smithnumber(n):
+    i = 2
+    l = []
+    while(n>1):
+        if isprime(i):
+            while(n%i == 0):
+                l.append(i)
+                n //= i
+        i += 1
+    print(l)
+    if 4==sum1(l):
+        return True
+    return False
+
+def fun_nth_smithnumber(n):
+    i = -1
+    j = 2
+    return smithnumber(22)
+    while(i != n):
+        print(j)
+        if smithnumber(j):
+            i += 1
+        j += 1
+    return j-1
+print(fun_nth_smithnumber(1))
