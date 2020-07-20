6bdfddbb2c1b5ac871a92fc2e6f893ff4a7e16ce
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-21 05:02:43 +0530

    Jul 21  05:02:39

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -35,7 +35,7 @@ def smithnumber(n):
                 l.append(i)
                 n //= i
         i += 1
-    if 22 == sum1(l):
+    if 4 == sum1(l):
         # print(sum1(l), l)
         return True
     return False
