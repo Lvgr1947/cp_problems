e59d317049e9f43c92b47adb8e2ed63e8aecbf54
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-20 21:17:06 +0530

    Jul 20  21:17:04

--- 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
+++ 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
@@ -28,20 +28,20 @@ def search(L,i,j,v):
 		l.append(a)
 		return l
 	elif ord(v) < ord(L[mid]):
+		print(ord(L[mid]))
 		a = tuple((mid,L[mid]))
 		l.append(a)
 		return search(L,i,mid,v)
 	elif ord(v) > ord(L[mid]):
-		i = mid
 		a = tuple((mid,L[mid]))
 		l.append(a)
 		return search(L,mid,j,v)
 	return l
 def recursion_binarysearchvalues(L, v):
 	i = 0
-	j=len(L)
+	j=len(L)-1
 	return search(L,i,j,v)
-print(recursion_binarysearchvalues(['a', 'c', 'f', 'g', 'm', 'q'],'m'))
+print(recursion_binarysearchvalues(['a', 'c', 'f', 'g', 'm', 'q'],'a'))
 
 
 	
