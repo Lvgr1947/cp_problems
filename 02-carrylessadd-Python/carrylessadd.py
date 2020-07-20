fd8eba979dad135afb5e9353c3b2d4769c92c0d8
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-21 03:07:29 +0530

    Jul 21  03:07:25

--- 02-carrylessadd-Python/carrylessadd.py
+++ 02-carrylessadd-Python/carrylessadd.py
@@ -13,7 +13,5 @@ def fun_carrylessadd(x, y):
 		if c >9: c -= 10
 		if c != 0: sum = sum + c*pow(10,i)
 		x,y,i = x//10,y//10, i+1
-
-	sum += x*pow(10,len(str(sum))) + y*pow(10,len(str(sum)))
-	return sum
+	return sum + x*pow(10,len(str(sum))) + y*pow(10,len(str(sum)))
 print(fun_carrylessadd(785, 376))
