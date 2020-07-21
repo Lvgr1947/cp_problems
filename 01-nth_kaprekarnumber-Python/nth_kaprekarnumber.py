118b6d20a55dce5dda8c000c4c78c75a059bc999
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-21 20:44:05 +0530

    Jul 21  20:44:02

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -11,9 +11,9 @@ import math
 def knumber(x):
     y = x**2
     y1 = y // (10**(((len(str(y)))//2)+1))
-    # print( "before",y1)
+    print( "before",y1)
     y2 = y % (10**(len(str(y))//2))
-    # print("after",y1)
+    print("after",y2)
     y2 = y1+y2
     if x == y2:
         print("knumber",x)
