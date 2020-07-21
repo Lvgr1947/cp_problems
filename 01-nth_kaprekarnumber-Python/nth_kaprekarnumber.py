4fbe98f9d607a061d8c6976c5fd1d1211f9f0443
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-21 20:20:36 +0530

    Jul 21  20:20:34

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -11,7 +11,7 @@ import math
 def knumber(x):
     y = x**2
     print((10**((len(str(y))//2))))
-    y1 = y // (10**((len(str(y))-1//2)))
+    y1 = y // (10**((len(str(y))//2)))
     print( "before",y1)
     y1 += y % (10**(len(str(y))//2))
     print("after",y1)
