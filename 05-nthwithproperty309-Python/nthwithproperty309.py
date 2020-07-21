0488ef13148ee85a94005a94e0e9a069eaffd4c1
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-21 05:44:45 +0530

    Jul 21  05:44:42

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -3,7 +3,15 @@
 # digit (from 0 to 9) at least once. 309 is the smallest number with this property. 
 # Write the function nthWithProperty309 that takes a non-negative int n and returns 
 # the nth number with Property309.
-
+def lookandsay(a):
+	c=1
+	l = []
+	for i in range(1,len(a)+1):
+		if i<len(a) and a[i-1] == a[i]:
+			c += 1
+		else:
+			l.append((c,a[i-1]))
+			c = 1
+	return(l)
 def nthwithproperty309(n):
-	# Your code goes here
-	pass
+	l = lookandsay(n**5)
