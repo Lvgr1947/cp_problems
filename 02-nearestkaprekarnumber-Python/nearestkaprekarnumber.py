729815da1c82cb41b313514bdbdaa909a63c6344
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-22 06:02:15 +0530

    Jul 22  06:02:11

--- 02-nearestkaprekarnumber-Python/nearestkaprekarnumber.py
+++ 02-nearestkaprekarnumber-Python/nearestkaprekarnumber.py
@@ -30,7 +30,7 @@ def fun_nearestkaprekarnumber(n):
     i , j =  0 , 2
     if n == 0:
         return 1
-    while(i<n):
+    while(True):
         if knumber(j):
             i += 1
         j += 1
