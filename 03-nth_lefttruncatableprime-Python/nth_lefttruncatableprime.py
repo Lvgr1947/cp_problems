8cbe450466145c44c6cf9994107bd42c7969dbe3
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-22 06:33:44 +0530

    Jul 22  06:33:40

--- 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
+++ 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
@@ -5,10 +5,25 @@
 # There are total 4260 left-truncatable primes.
 # So nthLeftTruncatablePrime(0) retunearestKaprekarNumber(n)rns 2, and 
 # nthLeftTruncatablePrime(10) returns 53.
-
+def isprime(n):
+	if n > 1:
+		for i in range(2,n):
+			if n % i ==0:
+				return False
+		else:
+			return True
+def istrue(n):
+    if n < 10:
+        return n
+    n = str(n)
+    n = int(n[1:len(n)])
+    if isprime(n):
+        return True
+    return False
 
 
 import math
 
 def fun_nth_lefttruncatableprime(n):
-    return 1
+    i,j = 2,0
+    whil
