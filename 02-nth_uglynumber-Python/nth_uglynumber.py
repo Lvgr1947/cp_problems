7b33ea84e346cb7ccc0ef439f623d850b3f10fb4
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-24 00:58:38 +0530

    Jul 24  00:58:34

--- 02-nth_uglynumber-Python/nth_uglynumber.py
+++ 02-nth_uglynumber-Python/nth_uglynumber.py
@@ -39,4 +39,6 @@ print(fun_nth_uglynumber(0))
 print(fun_nth_uglynumber(1))
 print(fun_nth_uglynumber(2))
 print(fun_nth_uglynumber(3))
+
+print(fun_nth_uglynumber(10))
 print(fun_nth_uglynumber(9))
