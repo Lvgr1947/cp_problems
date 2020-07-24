c9e3561f04d34a214c0ac2cff403fc1cf09dd37b
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-07-24 23:19:55 +0530

    Jul 24  23:19:52

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -19,10 +19,9 @@ def longestcommonsubstring(s1, s2):
     for i in range(l1):
         if s1[i] in s2:
             c2 = 1
-            j = s2.index(s2[i])
             e = s2[i]
-            for k in range(j+1,l2):
-                if s1[k] == s2[i+k-j]:
+            for k in range(0,l2):
+                if s1[k] == s2[k]:
                     c2 += 1
                     e += s1[k]
                 else:
